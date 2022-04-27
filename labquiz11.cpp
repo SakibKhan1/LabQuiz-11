6ead63bbc47c5d1a19ca289ba50b8cb869938d3e
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-04-27 01:04:58 -0400

    Create labquiz11.cpp

---
+++ labquiz11.cpp
@@ -0,0 +1,17 @@
+//Name: Sakib Khan  
+//CSCI 135
+//Lab Quiz 11
+
+#include <iostream>
+#include <cassert>
+#include <cstdlib>
+#include <fstream>
+using namespace std;
+
+int x = row + col;
+if(x % 2 == 0) {
+    out[row][col] = 1;
+} 
+else {
+    out[row][col] = 0;
+}
